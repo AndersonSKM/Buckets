3f00fce94d37153f3337ce7ec7a1d9a110c058c7
AndersonSKM <anderson.krs95@gmail.com>
2018-05-05 17:37:05 -0300

    Fix codacy warnings

--- auth/run.sh
+++ auth/run.sh
@@ -7,13 +7,12 @@ test_postgresql() {
 }
 
 count=0
-
 until ( test_postgresql )
 do
     count=$((count+1))
     if [ ${count} -gt 62 ]
     then
-        echo "Postgres didn't up in a time"
+        echo "$(date) - Postgres didn't up in a time"
         exit 1
     fi
     sleep 1
@@ -24,14 +23,12 @@ psql postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT 
     CREATE DATABASE $POSTGRES_DB ENCODING 'UTF-8' OWNER $POSTGRES_USER;
 EOSQL
 
-# Initialize Django project
+echo "Setup django..."
 python3 manage.py makemigrations
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
 
 echo "Starting WSGI service application..."
-
-# Run Gunicorn
 if [ "$DEBUG" == "true" ]; then
   /usr/local/bin/gunicorn auth.wsgi -b 0.0.0.0:8000 --reload
 else
--- mailer/run.sh
+++ mailer/run.sh
@@ -4,18 +4,17 @@ rabbitmq_is_ready() {
     eval "curl -I http://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@${RABBITMQ_HOST}:${RABBITMQ_MANAGEMENT_PORT}/api/vhosts"
 }
 
-i=0
-while ! rabbitmq_is_ready; do
-    i=`expr $i + 1`
-    if [ $i -ge 10 ]; then
-        echo "$(date) - rabbit still not ready, giving up"
+count=0
+until ( rabbitmq_is_ready )
+do
+    count=$((count+1))
+    if [ ${count} -gt 62 ]
+    then
+        echo "$(date) - RabbitMQ didn't up in a time"
         exit 1
     fi
-    echo "$(date) - waiting for rabbit to be ready"
-    sleep 3
+    sleep 1
 done
 
 echo "Starting Nameko Services..."
-
-# Run Nameko
 nameko run --config=settings.yaml mailer.services
