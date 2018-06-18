9168c6677ed37684f6c9893ea223dc3686935c39
AndersonSKM <anderson.krs95@gmail.com>
2018-06-18 10:20:06 -0300

    Fix api docs permissions

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from rest_framework.documentation import include_docs_urls
-from rest_framework import routers
+from rest_framework.permissions import AllowAny
 
-API_DOCS_TITLE = 'Buckets API'
+API_DOCS_URLS = include_docs_urls(
+    title='Buckets API',
+    permission_classes=(AllowAny,),
+    public=True
+)
 
 urlpatterns = [
-    path('api/docs/', include_docs_urls(title=API_DOCS_TITLE, public=True)),
+    path('api/docs/', API_DOCS_URLS),
     path('api/', include(('accounts.urls', 'accounts'), namespace='auth')),
 ]
