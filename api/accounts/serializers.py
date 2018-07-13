dec8185ca777f68980dc2fb77b485b302b19dc17
AndersonSKM <anderson.krs95@gmail.com>
2018-07-13 08:47:35 -0300

    test api exception when create user from api

--- api/accounts/serializers.py
+++ api/accounts/serializers.py
@@ -1,8 +1,10 @@
+from typing import Any, Dict
+
 from rest_framework import serializers
 
 from accounts.models import User
 
-default_user_fields = (
+USER_FIELDS = (
     'uri',
     'pk',
     'email',
@@ -16,7 +18,7 @@ default_user_fields = (
     'updated_at',
 )
 
-create_user_fields = default_user_fields + (
+CREATE_USER_FIELDS = USER_FIELDS + (
     'password',
     'password_confirm',
 )
@@ -35,7 +37,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = default_user_fields
+        fields = USER_FIELDS
 
 
 class FullUserSerializer(UserSerializer):
@@ -48,25 +50,19 @@ class UserCreateSerializer(UserSerializer):
 
     class Meta:
         model = User
-        fields = create_user_fields
+        fields = CREATE_USER_FIELDS
 
-    def validate(self, data: dict) -> dict:
+    def validate(self, data: Dict[str, Any]) -> Dict[str, Any]:
         if data['password'] != data['password_confirm']:
             raise serializers.ValidationError("Passwords don't match.")
         return data
 
-    def create(self, validated_data: dict) -> User:
+    def create(self, validated_data: Dict[str, Any]) -> User:
         email = validated_data.pop('email', None)
         password = validated_data.pop('password', None)
         validated_data.pop('password_confirm', None)
         is_staff = validated_data.pop('is_staff', False)
-
-        return User.objects.create_user(
-            email,
-            password,
-            is_staff,
-            **validated_data
-        )
+        return User.objects.create_user(email, password, is_staff, **validated_data)
 
 
 class FullUserCreateSerializer(UserCreateSerializer):
