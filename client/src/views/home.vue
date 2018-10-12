2792ee73995bf82115638842a8e77ab93acfb52b
Anderson Macedo <anderson.krs95@gmail.com>
2018-10-12 14:46:26 -0300

    Remove trash code

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <p>Home</p>
-    <v-btn @click="teste">Teste</v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  methods: {
-    teste () {
-      console.log('teste')
-      this.$router.push('/home')
-    }
-  }
+  name: 'Home'
 }
 </script>
 
