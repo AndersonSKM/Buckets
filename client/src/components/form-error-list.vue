ac4b7d1d8b448c4e5a081821999418e38bbb4ce8
Anderson Macedo <anderson.krs95@gmail.com>
2019-02-21 08:56:29 -0300

    Extract a component to render API non field errors

---
+++ client/src/components/form-error-list.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout align-center mt-3 mb-3 ml-4>
+    <ul data-ref="error-list">
+      <li
+        class="title font-weight-light red--text"
+        v-for="(error, index) in nonFieldErrors"
+        :key="index"
+      >
+        {{ error }}
+      </li>
+    </ul>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'FormErrorList',
+  computed: {
+    nonFieldErrors () {
+      return this.errors.items
+        .filter(error => error.field === 'non_field_errors')
+        .map(error => error.msg)
+    }
+  }
+}
+</script>
