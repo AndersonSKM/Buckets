589ed943d2a1d935743f07edbc7f1e31106a39c5
Anderson Macedo <anderson.krs95@gmail.com>
2019-03-02 09:30:36 -0300

    Import only used components from vuetify

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,6 +1,37 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vuetify, {
+  VApp,
+  VContent,
+  VNavigationDrawer,
+  VFooter,
+  VToolbar,
+  VFadeTransition,
+  VLayout,
+  VFlex,
+  VBtn,
+  VAlert,
+  VTextField,
+  VForm,
+  VAvatar,
+  VImg
+} from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VContent,
+    VNavigationDrawer,
+    VFooter,
+    VToolbar,
+    VFadeTransition,
+    VLayout,
+    VFlex,
+    VBtn,
+    VAlert,
+    VTextField,
+    VForm,
+    VAvatar,
+    VImg
+  }
 })
